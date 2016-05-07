387d76b6e3f96e21b84a3451788ee1e52b5dd436
Thomas Kotzian <thomas.kotzian@gmx.at>
2016-05-07 21:13:16 +0200

    update regular expressions to catch mode flags after +

--- lib/bind_log_analyzer/base.rb
+++ lib/bind_log_analyzer/base.rb
@@ -50,9 +50,9 @@ module BindLogAnalyzer
     # @return [Hash, false] The hash containing the parsed line or false if the line couldn't be parsed
     def parse_line(line)
       query = {}
-      regexp =     /^(\d{2}-\w{3}-\d{4}\s+\d{2}:\d{2}:\d{2}\.\d{3})\s+client\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})#\d+\s+\(.+\):\s+query:\s+(.*)\s+IN\s+(\w+)\s+\+\s+\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)$/
+      regexp =     /^(\d{2}-\w{3}-\d{4}\s+\d{2}:\d{2}:\d{2}\.\d{3})\s+client\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})#\d+\s+\(.+\):\s+query:\s+(.*)\s+IN\s+(\w+)\s+\+\w*\s+\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)$/
       old_regexp = /^(\d{2}-\w{3}-\d{4}\s+\d{2}:\d{2}:\d{2}\.\d{3})\s+client\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})#\d+:\s+query:\s+(.*)\s+IN\s+(\w+)\s+\+\s+\((\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\)$/
-      regexp_ipv6 = /^(\d{2}-\w{3}-\d{4}\s+\d{2}:\d{2}:\d{2}\.\d{3})\s+client\s+([0-9a-fA-F:]+)#\d+\s+\(.+\):\s+query:\s+(.*)\s+IN\s+(\w+)\s+\+\s+\(([0-9a-fA-F:]+)\)$/
+      regexp_ipv6 = /^(\d{2}-\w{3}-\d{4}\s+\d{2}:\d{2}:\d{2}\.\d{3})\s+client\s+([0-9a-fA-F:]+)#\d+\s+\(.+\):\s+query:\s+(.*)\s+IN\s+(\w+)\s+\+\w*\s+\(([0-9a-fA-F:]+)\)$/
 
 
       parsed_line = line.scan(regexp)
